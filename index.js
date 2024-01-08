c647c9ba13d7012106830f65785a25f4288ccc52
makhmudovdev <xursandmadiyarov2004@gmail.com>
2024-01-08 22:49:25 +0500

    masalalar

---
+++ index.js
@@ -0,0 +1,325 @@
+                // 3-masala
+
+
+// let number = +prompt("Please enter a number");
+// if (Number.isInteger(number) && number % 2 == 0) {
+//     console.log ("juft");
+// }else{
+//     console.log ("toq");
+// }
+
+
+
+
+                 // 4-masala
+
+// let number = +prompt("Please enter a number");
+// let number1 = +prompt("Please enter a number2");
+// if (Number.isInteger(number) && Number.isInteger(number1) &&  number>0 && number1>0) {
+//     console.log ("musbat");
+// }else{
+//     console.log ("manfiy");
+// }
+
+
+
+
+                  // 5-masala
+
+// let a = +prompt("Please enter a ");
+// let b = +prompt("Please enter b ");
+// let c = +prompt("Please enter c ");
+
+// if (a > b && b > c) {
+//     console.log("Rostlik mavjud: a > b > c");
+// } else {
+//     console.log("Rostlik yo'q");
+// }
+
+
+
+
+                               // 6-masala
+
+// let a = +prompt("Please enter a ");
+// let b = +prompt("Please enter b ");
+// let c = +prompt("Please enter c ");
+
+
+// if ((a < b && b < c) || (c < b && b < a)) {
+//     console.log("b soni a va c sonlari orasida yotadi.");
+// } else {
+//     console.log("b soni a va c sonlari orasida yotmaydi yoki noto'g'ri kiritilgan.");
+// }
+
+
+
+
+                            // 7-masala
+// let number = +prompt("Please enter a number");
+// let number1 = +prompt("Please enter a number2");
+// if (number% 2==1 && number1 % 2==1) {
+//     console.log ("toq");
+// }else{
+//     console.log ("juft");
+// }
+
+
+
+
+
+                           // 8-masala
+
+// let a = +prompt("please enter a ");
+
+// let b = +prompt("please enter b");
+
+// let c = +prompt("please enter c");
+
+// let d = +prompt("please enter d");
+
+// if(a > 0 && b > 0 && c > 0 && d > 0 ) {
+//     console.log("musbat son")
+// }else{
+//     console.log("manfiy son")
+// }
+
+
+
+                            // 9-masala
+
+// let a = +prompt("please enter a ");
+
+// let b = +prompt("please enter b");
+
+// let c = +prompt("please enter c");
+
+
+// if ((a > 0 && b > 0) || (b > 0 && c > 0) || (a > 0 && c > 0)) {
+//     console.log("Ha, kamida ikkita son musbat.");
+// } else {
+//     console.log("Yo'q, kamida ikkita son musbat emas.");
+// }
+
+
+
+
+                            // 10-masala
+
+// let a = +prompt("son kiriting");
+
+// if(a==1){
+//     console.log(`${a} - kun duyshanba`);
+// }else if(a==2){
+//     console.log(`${a} - kun siyshanba`);
+// }else if(a==3){
+//     console.log(`${a} - kun chorshanba`);
+// }else if(a==4){
+//     console.log(`${a} - kun payshanba`);
+// }else if(a==5){
+//     console.log(`${a} - kun juma`);
+// }else if(a==6){
+//     console.log(`${a} - kun shanba`);
+// }else if(a==7){
+//     console.log(`${a} - kun yakshanba`);
+// }else {
+//     console.log(`${a} - bunday hafta kuni yo'q`);
+// }
+
+                                // 11 -masala
+                                
+// let telefonKodi = prompt("Telefon raqam kodini kiriting:");
+
+// if (telefonKodi.startsWith("90")) {
+//     console.log("Beeline kompaniyasi");
+// } else if (telefonKodi.startsWith("93")) {
+//     console.log("Ucell kompaniyasi");
+// } else if (telefonKodi.startsWith("94")) {
+//     console.log("Ucell kompaniyasi");
+// } else if (telefonKodi.startsWith("97")) {
+//     console.log("Mobiuz kompaniyasi");
+// } 
+// else if (telefonKodi.startsWith("88")) {
+//     console.log("Mobiuz kompaniyasi");
+// }
+// else if (telefonKodi.startsWith("33")) {
+//     console.log("Humans kompaniyasi");
+// }else {
+//     console.log("Noma'lum kompaniya");
+// }
+
+
+
+
+                                // 12-masala
+
+// let a = +prompt("son kiriting");
+
+// if( Number.isInteger(a) && a > 0 ){
+//     console.log(++a);
+// }else if( Number.isInteger(a) && a < 0){
+//     console.log(--a);
+// }else {
+//     console.log("Iltimos butun son liriting !");
+// }
+
+
+
+
+                                //13 -masala
+ 
+// let a = +prompt("son kiriting");
+
+// if( Number.isInteger(a) && a > 0){
+//     console.log(+a +2);
+// }else if( Number.isInteger(a) && a < 0){
+//     console.log(a-2);
+// }else{
+//     console.log("iltimos qaytatdan butun son kiriting");
+// }
+
+
+
+
+
+                                // 14 - masala
+
+
+// let a = +prompt("a sonini kiriting :");
+// let b = +prompt("b sonini kiriting :");
+
+// if( Number.isInteger(a) && Number.isInteger(b) &&  a > b){
+//     console.log(`a = ${a} soni katta b = ${b}  sondam`);
+// }else if( Number.isInteger(a) && Number.isInteger(b) && a < b){
+//     console.log(`b = ${b} soni katta a = ${a}  sondam`);
+// }else {
+//     console.log("iltimos butun son kiriting");
+// }
+
+
+
+
+
+                                // 15 - masala
+
+// let a = +prompt("a sonini kiriting :");
+// let b = +prompt("b sonini kiriting :");
+// let c = +prompt("c sonini kiriting :");
+
+// if(Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c) && a > b && a > c){
+//     console.log(`siz kiritgan sonlar ichida eng kattasi a =  ${a}`);
+// }else if(Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c) && b > a && b > c){
+//     console.log(`siz kiritgan sonlar ichida eng kattasi b = ${b}`);
+// }else if(Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c) && c > a && c > b){
+//     console.log(`siz kiritgan sonlar ichida eng kattasi c = ${c}`);
+// }else{
+//     console.log("iltimos butun son kiriting");
+// }
+
+
+
+
+                                  // 16 - masala
+
+// let a = +prompt("a sonini kiriting :");
+// let b = +prompt("b sonini kiriting :");
+// let c = +prompt("c sonini kiriting :");
+
+// if(a < b && a < c){
+//     console.log(`siz kiritgan sonlar ichida eng kichkinasi a =  ${a}`);
+// }else if(b < a && b < c){
+//     console.log(`siz kiritgan sonlar ichida eng kichkinasi b =  ${b}`);
+// }else if(c < a && c < a){
+//     console.log(`siz kiritgan sonlar ichida eng kichkinasi c =  ${c}`);
+// }else{
+//     console.log("iltimos qayta tekshiring");
+// }
+
+
+
+
+                                    // 17 - masala
+
+/*let phone = +prompt('Telafon raqamingizni kiriting:');
+let phone2 = phone[0] + phone[1];
+
+if(phone2 == 99){
+    console.log(`Uzmobile nomer: ${phone}`);
+}else if(phone2 == 97){
+    console.log(`Mobiuz nomer: ${phone}`);
+}else if(phone2 == 94){
+    console.log(`Usell nomer: ${phone}`);
+}else if(phone2 == 90){
+    console.log(`Beeline nomer: ${phone}`);
+}else{
+    console.log("kechirasiz sinig ragamingiz kampaniyasi topilmadi , iltimos boshqa raqam kirgizing!");
+}*/ //ishlamadi qandaydit bug
+
+// let phone = prompt("Telefon raqamingizni kiriting:");
+
+// let phoneCode = phone[0] + phone[1];
+
+// if(phoneCode === "99"){
+//     console.log(`Uzmobile nomer: ${phone}`);
+// }else if(phoneCode === "97"){
+//     console.log(`Mobiuz nomer: ${phone}`);
+// }else if(phoneCode === "94"){
+//     console.log(`Usell nomer: ${phone}`);
+// }else if(phoneCode === "90"){
+//     console.log(`Beeline nomer: ${phone}`);
+// }else{
+//     console.log("kechirasiz sinig ragamingiz kampaniyasi topilmadi , iltimos boshqa raqam kirgizing!"); 
+// }
+
+
+
+
+                                // 18 - masala
+
+// let a = +prompt("Bohoyingizni kiritimg '5' '4' '3' '2' '1' ");
+
+
+// if(a == 5){
+//     console.log("Super stipendiya 🎉 1 500 000 so'm");
+// }else if(a == 4){
+//     console.log("Siznig stipeniyangiz 500 000 so'm");
+// }else if(a == 3){
+//     console.log("Sizning bahoyingiz stipendiya uchun noloyiq");
+// }else if(a == 2 || a == 1){
+//     console.log("Sizning  bahoyingiz qoniqarsiz");
+// }else{
+//     console.log("Iltimos mezom bo'yicha rost bahoyingizni kiriting !");
+// }
+
+
+
+
+                            // 19 - masala
+
+/*let a = +prompt(" Ihtiyori butun son kiriting");
+
+if (Number.isInteger(a) && a > 0 && a % 2 == 0){
+    console.log(`${a} soni , juft va musbat son`);
+}else if (Number.isInteger(a) && a < 0 && a % 2 == 0){
+    console.log(`${a} soni , juft va manfiy son`);
+}else if (Number.isInteger(a) && a < 0 && a % 2 != 0){
+    console.log(`${a} soni , tog va manfiy son`);
+}else if (Number.isInteger(a) && a > 0 && a % 2 != 0){
+    console.log(`${a} soni , tog va musbat son`);
+}else{
+    console.log("Iltimos butun son kiritin !");
+}*/
+
+
+                            // 20-masala
+
+// let year = prompt("Tug'ilgan yilingizni kiriting:");
+
+
+// let currentyear = new Date().getFullYear();
+
+
+// let yosh = currentyear - year;
+
+
+// console.log(`Siz ${yosh} yoshdasiz.`);
